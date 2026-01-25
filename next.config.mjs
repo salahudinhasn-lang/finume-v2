09bf988aa7365ce8bf64a89376a1350d09113ce8
salahudinhasn-lang <salahudinhasn@gmail.com>
2026-01-25 19:20:56 +0300

    fix(pwa): enable in dev and add ios install instructions

--- next.config.mjs
+++ next.config.mjs
@@ -22,7 +22,8 @@ const withPWA = withPWAInit({
     aggressiveFrontEndNavCaching: true,
     reloadOnOnline: true,
     swcMinify: true,
-    disable: process.env.NODE_ENV === "development",
+    // disable: process.env.NODE_ENV === "development",
+    disable: false, // Enable for testing
     workboxOptions: {
         disableDevLogs: true,
     },
